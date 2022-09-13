8fb04487a5e2703226219ce676a84de3b82a9554
Shlomo Moller <shlomo.moller@taboola.com>
2022-09-13 23:02:08 +0300

    Wrapp in a form just for fun

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <HhMmDurationInput />
+      <form>
+        <label htmlFor="duration-control">Duration:</label>
+        <HhMmDurationInput inputId='duration-control' />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
